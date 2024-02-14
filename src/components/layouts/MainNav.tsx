7b7df8c27e85af5551a66ccff1e1cf92ff37eee8
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-14 11:26:12 +0700

    feat: implement main navigation

--- src/components/layouts/MainNav.tsx
+++ src/components/layouts/MainNav.tsx
@@ -1,23 +1,32 @@
+import { IconType } from "react-icons";
+import {
+  HiOutlineCalendarDays,
+  HiOutlineCog8Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from "react-icons/hi2";
 import MainNavLink from "../ui/MainNavLink";
 
 const navLinks: {
   to: string;
   label: string;
+  icon: IconType;
 }[] = [
-  { to: "/dashboard", label: "Dashboard" },
-  { to: "/bookings", label: "Bookings" },
-  { to: "/cabins", label: "Cabins" },
-  { to: "/users", label: "Users" },
-  { to: "/settings", label: "Settings" },
+  { to: "/dashboard", label: "Dashboard", icon: HiOutlineHome },
+  { to: "/bookings", label: "Bookings", icon: HiOutlineCalendarDays },
+  { to: "/cabins", label: "Cabins", icon: HiOutlineHomeModern },
+  { to: "/users", label: "Users", icon: HiOutlineUsers },
+  { to: "/settings", label: "Settings", icon: HiOutlineCog8Tooth },
 ] as const;
 
 export default function MainNav() {
   return (
     <nav aria-label="Main navigation of the app">
       <ul className="flex flex-col gap-2">
-        {navLinks.map(({ to, label }) => (
-          <li key={to}>
-            <MainNavLink to={to}>{label}</MainNavLink>
+        {navLinks.map((linkItem) => (
+          <li key={linkItem.to}>
+            <MainNavLink {...linkItem} />
           </li>
         ))}
       </ul>
--- src/components/ui/MainNavLink.tsx
+++ src/components/ui/MainNavLink.tsx
@@ -1,19 +1,32 @@
 import { cn } from "@/lib/utils";
+import { IconType } from "react-icons";
 import { NavLink } from "react-router-dom";
 
-type MainNavLinkProps = React.ComponentProps<typeof NavLink>;
+interface MainNavLinkProps extends React.ComponentProps<typeof NavLink> {
+  icon: IconType;
+  label: string;
+}
 
-export default function MainNavLink({ className, ...props }: MainNavLinkProps) {
+export default function MainNavLink({
+  className,
+  icon,
+  label,
+  ...props
+}: MainNavLinkProps) {
   return (
     <NavLink
       className={({ isActive }) =>
         cn(
-          "flex px-5 py-4 font-medium rounded-sm hover:bg-gray-50",
-          isActive && "bg-gray-50",
+          "flex px-5 py-3.5 font-medium rounded-sm transition-colors",
+          "hover:bg-gray-50 [&:hover_svg]:stroke-brand-600",
+          isActive && "bg-gray-50 [&_svg]:stroke-brand-600",
           className,
         )
       }
       {...props}
-    />
+    >
+      {icon({ size: 24 })}
+      <span className="ml-3">{label}</span>
+    </NavLink>
   );
 }
