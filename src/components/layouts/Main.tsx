252536ee7ec8687a1d14010ec26bfe13f60e02df
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-15 14:51:12 +0700

    style: update layout

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Main() {
   return (
-    <main className="bg-green-500">
+    <main className="container py-6">
       <Outlet />
     </main>
   );
