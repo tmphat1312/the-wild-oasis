9455eb5538788d5e569b4bd4eaf8aab79f1ade54
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-24 12:47:08 +0700

    feat: add prefetching for pagination in useBookings

--- src/pages/bookings/useBookings.ts
+++ src/pages/bookings/useBookings.ts
@@ -1,11 +1,13 @@
+import { ITEMS_PER_PAGE } from "@/constants/API";
 import { queryKeys } from "@/constants/query-keys";
 import { useFilterField } from "@/hooks/useFilterField";
 import { usePagination } from "@/hooks/usePagination";
 import { useSortField } from "@/hooks/useSortField";
 import { getBookings } from "@/services/APIBookings";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const filterOption = useFilterField({
     filterField: "status",
     formatValue: (value) => value.toLowerCase().replace(/-/g, " "),
@@ -24,6 +26,37 @@ export function useBookings() {
     queryFn: () =>
       getBookings({ filterOptions: [filterOption], sortOption, page }),
   });
+
+  // PREFETCH
+  const noPages = Math.ceil(data.count / ITEMS_PER_PAGE);
+
+  if (noPages > 1) {
+    const nextPage = Math.min(page + 1, noPages);
+
+    queryClient.prefetchQuery({
+      queryKey: [...queryKeys.bookings, filterOption, sortOption, nextPage],
+      queryFn: () =>
+        getBookings({
+          filterOptions: [filterOption],
+          sortOption,
+          page: nextPage,
+        }),
+    });
+  }
+
+  if (page < noPages) {
+    const prevPage = Math.max(page - 1, 1);
+
+    queryClient.prefetchQuery({
+      queryKey: [...queryKeys.bookings, filterOption, sortOption, prevPage],
+      queryFn: () =>
+        getBookings({
+          filterOptions: [filterOption],
+          sortOption,
+          page: prevPage,
+        }),
+    });
+  }
 
   return { isLoading, error, bookings: data.bookings, count: data.count };
 }
