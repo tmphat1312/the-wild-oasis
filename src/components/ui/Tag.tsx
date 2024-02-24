0a24c318d982666d4368155c900106f83164d9e4
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-24 18:31:07 +0700

    fix(a11y): increase contrast ratio of text and background

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -4,11 +4,11 @@ import { tv } from "tailwind-variants";
 const colorVariants = tv({
   variants: {
     variant: {
-      success: "bg-green-100 text-green-600",
-      info: "bg-blue-100 text-blue-600",
-      warning: "bg-yellow-100 text-yellow-600",
-      danger: "bg-red-100 text-red-600",
-      muted: "bg-gray-100 text-gray-600",
+      success: "bg-green-100 text-green-800",
+      info: "bg-blue-100 text-blue-800",
+      warning: "bg-yellow-100 text-yellow-800",
+      danger: "bg-red-100 text-red-800",
+      muted: "bg-gray-200 text-gray-800",
     },
   },
 });
