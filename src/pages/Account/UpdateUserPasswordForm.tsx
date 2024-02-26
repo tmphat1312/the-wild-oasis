5503033c17bdb303f3a59e01909027270d57dd59
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-26 14:47:10 +0700

    feat: update user password

--- src/pages/Account/UpdateUserPasswordForm.tsx
+++ src/pages/Account/UpdateUserPasswordForm.tsx
@@ -2,81 +2,86 @@ import Section from "@/components/layouts/Section";
 import Heading from "@/components/ui/Heading";
 import { FieldSeparator } from "@/components/ui/form/FieldSeparator";
 import { useForm } from "react-hook-form";
-import { useUser } from "../auth/useUser";
-import { FullLoadingIndicator } from "@/components/ui/FullLoadingIndicator";
-import { useUpdateUserData } from "./useUpdateUserData";
+import { useUpdateUserPassword } from "./useUpdateUserPassword";
 
 interface UpdateUserDataFormData {
-  full_name: string;
+  newPassword: string;
 }
 
-export function UpdateUserDataForm() {
-  const { isLoading: isLoadingUser, user } = useUser();
-  const { isLoading: isUpdatingUserData, updateUserData } = useUpdateUserData();
+export function UpdateUserPasswordForm() {
+  const { isLoading: isUpdatingUserPassword, updateUserPassword } =
+    useUpdateUserPassword();
   const {
     handleSubmit,
     register,
     formState: { errors },
+    getValues,
   } = useForm({
     defaultValues: {
-      full_name: user?.user_metadata.full_name || "",
+      newPassword: "",
+      confirmPassword: "",
     },
   });
 
-  function onSubmit(data: UpdateUserDataFormData) {
-    updateUserData({ data });
-  }
-
-  if (isLoadingUser) {
-    return <FullLoadingIndicator />;
+  function onSubmit({ newPassword }: UpdateUserDataFormData) {
+    updateUserPassword({ newPassword });
   }
 
   return (
     <Section>
-      <Heading>Update user data</Heading>
+      <Heading>Update password</Heading>
       <form
         className="rounded-md border bg-background p-8"
         onSubmit={handleSubmit(onSubmit)}
       >
         <div className="grid grid-cols-[18ch_minmax(10ch,28ch)_1fr] items-center gap-4">
-          <label htmlFor="email" className="">
-            Email
-          </label>
+          <label htmlFor="new-password">New password</label>
           <input
-            type="email"
-            id="email"
+            type="password"
+            {...register("newPassword", {
+              required: "new password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+            id="new-password"
             className="w-full rounded-md border border-s-4 px-2.5 py-1.5"
-            disabled
-            value={user?.email}
+            disabled={isUpdatingUserPassword}
           />
+          <p className="text-sm text-red-500 first-letter:capitalize">
+            {errors.newPassword?.message}
+          </p>
         </div>
         <FieldSeparator />
         <div className="grid grid-cols-[18ch_minmax(10ch,28ch)_1fr] items-center gap-4">
-          <label htmlFor="full-name" className="">
-            Full name
-          </label>
+          <label htmlFor="confirm-password">Confirm password</label>
           <input
-            type="text"
-            {...register("full_name", { required: "Full name is required" })}
-            id="full-name"
+            type="password"
+            {...register("confirmPassword", {
+              required: "confirm password is required",
+              validate: (value) =>
+                value === getValues("newPassword") || "Passwords do not match",
+            })}
+            id="confirm-password"
             className="w-full rounded-md border border-s-4 px-2.5 py-1.5"
-            disabled={isUpdatingUserData}
+            disabled={isUpdatingUserPassword}
           />
           <p className="text-sm text-red-500 first-letter:capitalize">
-            {errors.full_name?.message}
+            {errors.confirmPassword?.message}
           </p>
         </div>
         <FieldSeparator />
         <div className="space-x-3 text-end">
           <button
-            disabled={isUpdatingUserData}
+            disabled={isUpdatingUserPassword}
             className="rounded border bg-gray-50 px-3.5 py-2.5 text-sm"
             type="reset"
           >
             Cancel
           </button>
           <button
-            disabled={isUpdatingUserData}
+            disabled={isUpdatingUserPassword}
             type="submit"
             className="rounded border bg-brand-600 px-3.5 py-2.5 text-sm text-brand-50"
           >
