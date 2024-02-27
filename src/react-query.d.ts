3102a27e7344b659796a2d9492ec7a8aa45e0c4d
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-27 20:11:04 +0700

    refactor: rollback to default error type

--- src/react-query.d.ts
+++
@@ -1,8 +0,0 @@
-import "@tanstack/react-query";
-import { PostgrestError } from "@supabase/supabase-js";
-
-declare module "@tanstack/react-query" {
-  interface Register {
-    defaultError: PostgrestError;
-  }
-}
