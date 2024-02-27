70ec13f87d53448c019d95a52724b892ff683288
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-27 20:36:22 +0700

    style: adjust heading font size

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -10,8 +10,8 @@ const headingVariants = tv({
   variants: {
     level: {
       h1: "text-3xl mb-6",
-      h2: "text-2xl mb-5",
-      h3: "text-1xl mb-4",
+      h2: "text-xl mb-4",
+      h3: "text-lg mb-4",
       h4: "text-lg mb-3",
       h5: "mb-2",
       h6: "text-sm mb-1",
@@ -22,7 +22,7 @@ const headingVariants = tv({
   },
 });
 
-const HeadingVariant = z.enum(["h1", "h2", "h3", "h4", "h5", "h6"] as const);
+const HeadingVariant = z.enum(["h1", "h2", "h3", "h4", "h5", "h6"]);
 
 export default function Heading({ className, ...props }: HeadingProps) {
   const level = useLevelContext();
