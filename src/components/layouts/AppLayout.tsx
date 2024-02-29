f70de5bdbda806595511de35de62b593a537beeb
Truong Minh Phat <tmphat1312@gmail.com>
2024-02-29 14:17:46 +0700

    fix: remove double scrollbar

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -4,9 +4,9 @@ import { Sidebar } from "./Sidebar";
 
 export function AppLayout() {
   return (
-    <div className="grid h-dvh grid-cols-[auto_1fr] overflow-x-clip bg-gray-100">
+    <div className="grid h-dvh grid-cols-[auto_1fr] overflow-hidden  bg-gray-100">
       <Sidebar className="row-span-full" />
-      <div className="container relative max-h-full space-y-6 overflow-y-auto px-12 pb-6 pt-3">
+      <div className="container grid max-h-full grid-rows-[auto_1fr] space-y-6 overflow-y-auto px-12 pb-6 pt-3">
         <Header />
         <Main />
       </div>
