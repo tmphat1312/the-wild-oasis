88bf8db2354b852ed5c369edb60afa1e3441cc74
Truong Minh Phat <tmphat1312@gmail.com>
2024-03-03 11:49:48 +0700

    fix: remove env variables without VITE_ preview for local build

--- src/services/APIClient.ts
+++ src/services/APIClient.ts
@@ -1,12 +1,11 @@
 import { Database } from "@/types/database";
 import { createClient } from "@supabase/supabase-js";
 
-const { VITE_SUPABASE_URL, VITE_SUPABASE_KEY, MODE, SUPABASE_KEY } = import.meta
-  .env;
+const { VITE_SUPABASE_URL, VITE_SUPABASE_KEY } = import.meta.env;
 
 export const APIClient = createClient<Database>(
   VITE_SUPABASE_URL,
-  MODE === "development" ? VITE_SUPABASE_KEY : SUPABASE_KEY,
+  VITE_SUPABASE_KEY,
 );
 
 export const buildStorageAPIClient = () => APIClient.storage;
